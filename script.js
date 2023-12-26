173d281e4278714e5a28cceb2c556301dbcbe853
robertojff <robertojff3333@gmail.com>
2023-12-26 18:36:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let valorFonte = 210;
+
+valorFonte += valorFonte;
+console.log(valorFonte);
+
+let valorSsd = 110;
+
+valorSsd -= valorSsd;
+console.log(valorSsd);
+
+let valorPlacaVideo = 1200;
+
+valorPlacaVideo **= valorPlacaVideo;
+console.log(valorPlacaVideo);
+
+let valorMonitor = 550;
+
+valorMonitor += valorMonitor;
+console.log(valorMonitor);
+
+let valorGabinete = 120;
+
+valorGabinete -= valorGabinete;
+console.log(valorGabinete);
+
+let valorMouse = 79;
+
+valorMouse *= valorMouse;
+console.log(valorMouse);
+
+
+let valorTecladoGamer = 100;
+
+valorTecladoGamer += valorTecladoGamer;
+console.log(valorTecladoGamer);
